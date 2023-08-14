c444fb6bdd2f72c1d3f53d493b398b393c53b398
pixelbyaj <abhishek_joshi@epam.com>
2023-08-14 16:32:34 +0200

    useHash: true

--- projects/ngx-iso-app/src/app/app.module.ts
+++ projects/ngx-iso-app/src/app/app.module.ts
@@ -7,7 +7,7 @@ import { TranslateHttpLoader } from '@ngx-translate/http-loader';
 import { TranslateLoader, TranslateModule } from '@ngx-translate/core';
 // import { NgxIsoFormModule } from 'projects/ngx-iso-form/src/public-api';
 import { AngularSplitModule } from 'angular-split';
-import {MatListModule} from '@angular/material/list';
+import { MatListModule } from '@angular/material/list';
 import { RouterModule } from '@angular/router';
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
@@ -15,7 +15,7 @@ import { NgxIsoFormModule } from 'ngx-iso-form';
 
 // AoT requires an exported function for factories
 export function HttpLoaderFactory(http: HttpClient) {
-  return new TranslateHttpLoader(http);
+  return new TranslateHttpLoader(http, './assets/i18n/', '.json');
 }
 @NgModule({
   declarations: [
@@ -30,16 +30,16 @@ export function HttpLoaderFactory(http: HttpClient) {
     MatIconModule,
     MatButtonModule,
     RouterModule.forRoot([
-      {path: 'xsd/:json', component: AppComponent, pathMatch: 'full' },
-      {path: '**', component: AppComponent}
-    ]),
+      { path: 'xsd/:json', component: AppComponent, pathMatch: 'full' },
+      { path: '**', component: AppComponent }
+    ], { useHash: true }),
     TranslateModule.forRoot({
-      defaultLanguage:'en',
+      defaultLanguage: 'en',
       loader: {
         provide: TranslateLoader,
         useFactory: HttpLoaderFactory,
         deps: [HttpClient]
-    }
+      }
     }),
   ],
   providers: [],
